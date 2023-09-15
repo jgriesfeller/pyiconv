52d2c6cb2b8b631ae9f634083318e0f7d5721201
Jan Jurgen Griesfeller <jan.griesfeller@met.no>
2023-09-15 12:57:45 +0200

    2nd working version

--- src/pyiconv/cli/pyiconv.py
+++ src/pyiconv/cli/pyiconv.py
@@ -7,24 +7,24 @@ encodings only available with libiconv's --enable-extra-encodings compile flag
 from __future__ import annotations
 
 import argparse
-import codecs
 import sys
 
-from pyiconv.lib import ENCODINGS, DEFAULT_TO_ENCODING, DEFAULT_FROM_ENCODING
+from ..lib import ENCODINGS, DEFAULT_TO_ENCODING, DEFAULT_FROM_ENCODING
 
 
 def main():
     parser = argparse.ArgumentParser(
         description="Python based 'repacement' for iconv; output will be written to stdout."
     )
-    parser.add_argument("files", help="file(s) to read", nargs="?")
+    parser.add_argument("files", help=" to read", nargs="?")
     parser.add_argument(
-        "-f", "--from-code", help="Specifies the encoding of the input.",
-        default=DEFAULT_FROM_ENCODING
+        "-f",
+        "--from-code",
+        help="Specifies the encoding of the input.",
+        default=DEFAULT_FROM_ENCODING,
     )
     parser.add_argument(
-        "-t", "--to-code", help="Specifies the encoding of the input.",
-        default=DEFAULT_TO_ENCODING
+        "-t", "--to-code", help="Specifies the encoding of the input.", default=DEFAULT_TO_ENCODING
     )
     parser.add_argument("--list", help="list encodings and exit.", action="store_true")
 
@@ -45,8 +45,9 @@ def main():
         options["to_code"] = args.to_code
 
     for _file in options["files"]:
+        # reconfigure stdout to new encoding
+        sys.stdout.reconfigure(encoding=options["to_code"], errors="ignore")
         with open(_file, "r", encoding=options["from_code"]) as infile:
-
             for line in infile:
                 print(line.strip())
 
